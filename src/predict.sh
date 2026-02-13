0b286b30b429b53a2ca372efe273b40274fcf415
Vincent Chau <43220811+vincentczhou@users.noreply.github.com>
2026-02-13 01:09:07 -0800

    fix

--- src/predict.sh
+++ src/predict.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 set -v
-uv run python src/myprogram.py test --work_dir work --test_data $1 --test_output $2
+uv run --no-dev python src/myprogram.py test --work_dir work --test_data $1 --test_output $2
